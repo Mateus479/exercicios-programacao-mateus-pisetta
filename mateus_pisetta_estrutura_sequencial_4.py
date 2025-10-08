3b655a97bcfb481dd311d81e66f7ad83766671ea
Mateus479 <mateus_pisetta@estudante.sesisenai.org.br>
2025-10-07 21:20:27 -0300

    Create mateus_pisetta_estrutura_sequencial_4.py

---
+++ mateus_pisetta_estrutura_sequencial_4.py
@@ -0,0 +1,10 @@
+#faça um programa que peça as 4 notas bimestrais e mostre a média
+nota_1=float(input('digite a nota 1: '))
+nota_2=float(input('digite a nota 2: '))
+nota_3=float(input('digite a nota 3: '))
+nota_4=float(input('digite a nota 4: '))
+média=(nota_1 + nota_2 + nota_3 + nota_4) /4
+print(f'média {média}')
+
+
+
